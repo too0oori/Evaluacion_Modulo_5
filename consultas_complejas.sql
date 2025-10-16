b70ab10f895886906edd15dd8729d4218311cf86
too0oori <sofia.lagos.cesped@gmail.com>
2025-10-15 23:40:28 -0300

    se añade modelo erd

---
+++ consultas_complejas.sql
@@ -0,0 +1,10 @@
+
+
+Consultas Complejas
+
+    Realiza una consulta que recupere el total de ventas de un producto durante el mes anterior.
+
+    Utiliza JOINs (INNER, LEFT) para obtener información relacionada entre las tablas productos, proveedores y transacciones.
+
+    Implementa una consulta con subconsultas (subqueries) para obtener productos que no se han vendido durante un período determinado.
+
---
+++ transacciones.sql
@@ -0,0 +1,10 @@
+
+
+Transacciones SQL
+
+    Realiza una transacción para registrar una compra de productos. Utiliza el comando BEGIN TRANSACTION, COMMIT y ROLLBACK para asegurar que los cambios se apliquen correctamente.
+
+    Asegúrate de que los cambios en la cantidad de inventario y las transacciones se realicen de forma atómica.
+
+    Utiliza el modo AUTOCOMMIT para manejar operaciones individuales si es necesario.
+
