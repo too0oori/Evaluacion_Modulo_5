330fdba10caadc9f2bbd656dcd31d4c3dcb8241d
too0oori <sofia.lagos.cesped@gmail.com>
2025-10-16 15:28:26 -0300

    se realiza transacciones de una compra de productos

--- transacciones.sql
+++ transacciones.sql
@@ -1,10 +1,23 @@
-
-
 -- Transacciones SQL
-
+USE sistema_inventario_db;
   --  Realiza una transacción para registrar una compra de productos. Utiliza el comando BEGIN TRANSACTION, COMMIT y ROLLBACK para asegurar que los cambios se apliquen correctamente.
+START TRANSACTION;
+--bloqueo de fila para evitar condiciones de carrera
+SELECT stock FROM productos WHERE id = 1 FOR UPDATE; 
+INSERT INTO transaccion (producto_id, proveedor_id, tipo, fecha, cantidad)    --  Asegúrate de que los cambios en la cantidad de inventario y las transacciones se realicen de forma atómica.
+VALUES (1, 1, 'COMPRA', '2024-09-01', 5);
 
-  --  Asegúrate de que los cambios en la cantidad de inventario y las transacciones se realicen de forma atómica.
+UPDATE productos
+SET stock = stock + 5
+WHERE id = 1;
+
+COMMIT;
+
+-- Verificación de la transacción
+SELECT * FROM transaccion WHERE producto_id = 1 ORDER BY id DESC;
+SELECT id, nombre, stock FROM productos WHERE id = 1;
+
+
 
   --  Utiliza el modo AUTOCOMMIT para manejar operaciones individuales si es necesario.
 
